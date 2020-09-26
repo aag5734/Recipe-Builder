fdeff492d0bf7a2c26450f61c31cad121383da3f
aag5734 <69917471+aag5734@users.noreply.github.com>
2020-09-26 10:27:48 -0700

    Update recipe_builder.py

--- recipe_builder.py
+++ recipe_builder.py
@@ -1,3 +1,15 @@
-"""
-this is a test
-"""
+def list_create(list):
+    ingredients = ["apple", "test", "test_again"]
+    for i in range(len(list)):
+        if list[i] == ',':
+            ingredients[0] = list[0:i]
+    print(ingredients[0])
+
+
+def main():
+    ingredient_list = input("Input list of different ingredients (a, b, c): ")
+    list_create(ingredient_list)
+
+
+if __name__ == "__main__":
+    main()
