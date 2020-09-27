87cdc103e05d045b3da8086cb4a1852ae961ca10
jlz6146 <69858638+jlz6146@users.noreply.github.com>
2020-09-26 21:53:48 -0400

    Update recipe_builder.py

--- recipe_builder.py
+++ recipe_builder.py
@@ -2,12 +2,22 @@ from dataclasses import dataclass
 
 
 def list_create(list):
+    """
+    creates list with user input
+    :param list: user inputted list
+    :return: list of ingredients
+    """
     ingredients = list.split(', ')
     return sorted(ingredients)
 
 
 @dataclass()
 class Recipe:
+    """
+    name: name of recipe
+    ingredients: ingredients list in recipe
+    instruct: recipe description
+    """
     name: str
     ingredients: list
     instruct: str
@@ -36,27 +46,33 @@ def readme(filename: str):
 
 
 def search(ingredience: list, resippy: list):
+    """
+    finds matching recipes
+    :param ingredience: ingredients list
+    :param resippy: recipe list
+    :return:
+    """
     show = []
     for entry in resippy:
         if len(ingredience) > len(entry.ingredients):
             result = all(elem in ingredience for elem in entry.ingredients)
             if result:
-                print(entry.name + ':')
-                print(entry.instruct)
+                show.append(entry)
         elif len(ingredience) == len(entry.ingredients):
             if ingredience == entry.ingredients:
-                print(entry.name + ':')
-                print(entry.instruct)
+                show.append(entry)
+    if show == []:
+        print("Sorry, there are no recipes available")
+    else:
+        for piece in show:
+            print("With these ingredients, you can make " + piece.name + " : " + piece.instruct)
 
 def main():
     ingredient_list = input("Input list of different ingredients (a, b, c): ")
     ingre = list_create(ingredient_list)
     resip = readme("rec1test.csv")
-    print(resip)
     search(ingre, resip)
 
 
 if __name__ == "__main__":
     main()
-
-
